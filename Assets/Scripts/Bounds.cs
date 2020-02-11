62ffaa848c65e7cff6903af2873a9fb2c8ae1efa
ArnauGarcis <arnaugallecs@gmail.com>
2020-02-11 17:11:54 +0100

    Programacion

---
+++ Assets/Scripts/Bounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour {
+
+    public GameObject m_Nave;
+
+	void Start () {
+
+		
+	}
+	
+	void Update () {
+
+        IntersectBounds(GetComponent<SpriteRenderer>(), m_Nave.GetComponent<SpriteRenderer>());
+	}
+
+
+    public bool IntersectBounds (SpriteRenderer l_Ball, SpriteRenderer l_Nave)
+    {
+        return l_Ball.bounds.max.y > l_Nave.bounds.min.y
+            && l_Ball.bounds.min.y < l_Nave.bounds.min.y
+            && l_Ball.bounds.max.x > l_Nave.bounds.min.x
+            && l_Ball.bounds.min.x < l_Nave.bounds.min.x;
+
+    }
+}
---
+++ Assets/Scripts/MovimientoNave.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoNave : MonoBehaviour {
+
+    public float m_Speed;
+    public GameObject m_Nave;
+    
+
+
+
+
+
+    void Start () {
+
+
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
