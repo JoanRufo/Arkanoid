3e035e440d17cacfa284d4d8579efd5e5cda5570
JoanRufo <jcarles2001@gmail.com>
2020-02-13 20:53:47 +0100

    Canvas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,21 +11,40 @@ public class GameManager : MonoBehaviour {
     public Transform SpawnBola;
     public Text m_textPuntuacion;
     public Text m_textVidas;
+    public bool playing = false;
+    float currentCD_Vidas;
+    public float CDMax_Vidas;
+    [HideInInspector] public int m_Puntuacion;
+    [HideInInspector] public int m_Vidas = 5;
 
-    void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
-
-    public void CrearBola(GameObject l_bola)
-    {
-        Instantiate(l_bola, SpawnBola.position, l_bola.transform.rotation);
-
+    void Start() {
 
     }
 
+    void Update()
+    {
+        if (playing == true)
+        {
+            if (currentCD_Vidas < CDMax_Vidas)
+            {
+                currentCD_Vidas += Time.deltaTime;
 
-}
+
+                m_textPuntuacion.text = "Puntuación:" + m_Puntuacion;
+                m_textVidas.text = "Vidas:" + m_Vidas;
+            }
+            if (currentCD_Vidas >= CDMax_Vidas)
+            {
+                currentCD_Vidas = 0;
+            }
+        }
+    }
+
+        public void CrearBola(GameObject l_bola)
+        {
+            Instantiate(l_bola, SpawnBola.position, l_bola.transform.rotation);
+
+
+        }
+    }
+
